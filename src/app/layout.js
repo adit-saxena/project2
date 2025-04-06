b3761101af9159b328edba1323071f3653e2b7f6
adit-saxena <aditsaxena2004@gmail.com>
2025-04-07 00:12:16 +0530

    hotjar fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -50,6 +50,18 @@ export default function RootLayout({ children }) {
             })(window, document, "clarity", "script", "${clarityProjectId}");
           `}
         </Script>
+        <Script id="hotjar-init" strategy="afterInteractive">
+  {`
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3861756,hjsv:6}; // Your specific Hotjar Site ID
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+  `}
+</Script>
 
         
 
